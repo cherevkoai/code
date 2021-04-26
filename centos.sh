17123a6b12f59e7878821f4f7a1a4cf02dc0f3ab
cherevkoai <82883784+cherevkoai@users.noreply.github.com>
2021-04-26 14:49:44 +0300

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -14,15 +14,15 @@ git clone https://github.com/cherevkoai/code.git
 sudo yum install -y pytho3 python3-pip
 
 #Install Docker-Ce
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker -y
 
 #Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo yum install jenkins java-1.8.0-openjdk-devel
+sudo yum -y upgrade
+sudo yum -y install jenkins java-1.8.0-openjdk-devel
 sudo systemctl daemon-reload
 
 #Install Postgres 9.2
