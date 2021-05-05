48e1d6de3d4ef3139781da2fa456cfc1e479d1f9
cherevkoai <82883784+cherevkoai@users.noreply.github.com>
2021-05-06 00:19:08 +0300

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -2,33 +2,37 @@
 
 sudo yum update -y
 
-sudo yum install -y nano
-sudo yum install -y git
-sudo yum install -y htop
-sudo yum install -y mc
-sudo yum install -y net-tools
-sudo yum install -y bind-utils
-sudo yum install -y wget
+#sudo yum install -y nano
+#sudo yum install -y git
+#sudo yum install -y htop
+#sudo yum install -y mc
+#sudo yum install -y net-tools
+#sudo yum install -y bind-utils
+#sudo yum install -y wget
 
-git clone https://github.com/cherevkoai/code.git
+#git clone https://github.com/cherevkoai/code.git
 
-sudo yum install -y pytho3 python3-pip
+#sudo yum install -y pytho3 python3-pip
 
 #Install Docker-Ce
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker -y
+#sudo yum install -y docker-ce docker-ce-cli containerd.io
+#sudo systemctl start docker -y
 
 #Install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum -y upgrade
-sudo yum -y install jenkins java-1.8.0-openjdk-devel
-sudo systemctl daemon-reload
+#sudo wget -O /etc/yum.repos.d/jenkins.repo \
+#    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#sudo yum -y upgrade
+#sudo yum -y install jenkins java-1.8.0-openjdk-devel
+#sudo systemctl start jenkins
+#systemctl status jenkins
+#sudo systemctl enable jenkins
+#sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+#sudo firewall-cmd --reload
 
 #Install Postgres 9.2
-sudo yum install postgresql postgresql-server
-sudo postgresql-setup initdb
-sudo systemctl enable postgresql && systemctl start postgresql
+#sudo yum install postgresql postgresql-server
+#sudo postgresql-setup initdb
+#sudo systemctl enable postgresql && systemctl start postgresql
 
-#sudo yum upgrade -y
+sudo yum upgrade -y
